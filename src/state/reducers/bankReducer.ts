7239b179e4e001334a60f98a1cfb89f126c7c9d6
Weiqifan1 <cmlykke@hotmail.com>
2021-09-28 21:50:11 +0200

    2021-09-28_21.49: basic routing works. I need to set a default page

--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -3,7 +3,7 @@ import {ActionType} from "../action-types";
 
 const initialState = 0;
 
-const reducer = (state: number = initialState, action: Action) => {
+const reducer = (state: number = initialState, action: Action): number => {
     switch (action.type){
         case ActionType.DEPOSIT:
             return state + action.payload;
