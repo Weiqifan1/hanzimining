50b4cf2240e808bcc26d7dc6c178236e9ba0bef1
Weiqifan1 <cmlykke@hotmail.com>
2021-10-03 18:55:14 +0200

    2021-10-03_18.53: dummy products are rendered dynamically. Now need to create a list of json objects with heisig info.

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,13 +2,24 @@ import IPage from "../interfaces/page";
 
 const Products: React.FunctionComponent<IPage> = props => {
 
+    const allProducts = [
+        {
+            name: "name1",
+            info: "info1",
+            price: 1
+        },
+        {
+            name: "name2",
+            info: "info2",
+            price: 2
+        }
+    ]
+
     return (
     <section>
         <h1> The Products page </h1>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
+            {allProducts.map(meal => <li>{meal.name}</li>)}
         </ul>
     </section>
     );
