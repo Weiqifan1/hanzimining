d00b3e4dee0472e4299de136feea4a3c22c40c58
Weiqifan1 <cmlykke@hotmail.com>
2021-10-15 12:06:23 +0200

    20211015_12.05: cleanedApp.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Route, Switch, RouteComponentProps} from 'react-router-dom';
+import {Route, Switch, RouteComponentProps} from 'react-router-dom';
 import routes from "./config/routes";
-import Welcome from "./pages/Welcome"//from "./components/Welcome";
-import Products from "./pages/Products";
-import Insurance from "./pages/Insurance";
 import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
 
 
 const App: React.FunctionComponent<{}> = props => {
@@ -37,22 +32,3 @@ const App: React.FunctionComponent<{}> = props => {
 }
 
 export default App;
-
-/*
-<MainHeader/>
-<main>
-            <Route path="/welcome">
-                <Welcome/>
-            </Route>
-            <Route path="/products">
-                <Products/>
-            </Route>
-            <Route path="/product-detail">
-                <ProductDetail/>
-            </Route>
-            <Route path="/insurance">
-                <Insurance/>
-            </Route>
-
-        </main>
-*/
