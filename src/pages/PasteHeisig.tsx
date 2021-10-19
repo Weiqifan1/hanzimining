bd0e76bc7d93aa2767905ef59423edf7aad7f225
Weiqifan1 <cmlykke@hotmail.com>
2021-10-19 12:27:10 +0200

    20211019_12.26: can read content of input field

--- src/pages/PasteHeisig.tsx
+++ src/pages/PasteHeisig.tsx
@@ -4,8 +4,21 @@ import IPage from "../interfaces/page";
 
 const PasteHeisig: React.FunctionComponent<IPage> = props => {
 
+    const processJsonInput = () => {
+        const content: string = ((document.getElementById("inserthanzi") as HTMLInputElement).value);
+        console.log("process json content");
+        console.log(content);
+        console.log(typeof content)
 
-    return <h1> The paste heisig page </h1>
+    }
+
+    return <section>
+        <h1> The paste heisig page </h1>
+        <button type="button" onClick={() => processJsonInput()}>processJson</button>
+        <p>***</p>
+        <input type="text" id="inserthanzi" placeholder="paste character json file content"></input>
+
+    </section>
 };
 
 export default PasteHeisig;
