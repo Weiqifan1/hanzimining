01d766ab56acd840036f6c046377c4bb57015ee7
Weiqifan1 <cmlykke@hotmail.com>
2021-10-20 13:07:07 +0200

    2021-10-20_1306: increased font size for characters

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,12 +1,13 @@
 import {Content} from "../state/state-types/charactersrstypes";
+import classes from "./TodoItem.module.css"
 
 const TodoItem: React.FC<{content: Content}> = (props) => {
 
     return <ul>
+        <li className={classes.characterListElement}>{props.content.character}</li>
         <li>{props.content.number}</li>
         <li>{props.content.reviewValue}</li>
         <li>{props.content.dateOfLastReview}</li>
-        <li>{props.content.character}</li>
         <li>{props.content.keyword}</li>
         <li>{props.content.story}</li>
         </ul>
