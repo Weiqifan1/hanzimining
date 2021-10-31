c2741e68f13c243205e86aa40c3e5b0b2e723575
Weiqifan1 <cmlykke@hotmail.com>
2021-10-31 14:33:23 +0100

    2021-10-31_1432: added more consistent logic to display characters logic

--- src/pages/DisplayAllHeisig.tsx
+++ src/pages/DisplayAllHeisig.tsx
@@ -12,40 +12,49 @@ const DisplayAllHeisig: React.FunctionComponent<IPage> = props => {
     const characterSRSstate = useSelector(
         (state: State) => state.characterSRS
     )
-    //const dispatch = useDispatch();
-    //const {createSRSobject} = bindActionCreators(characterSRSactionCreators, dispatch)
 
     function prepareNumberToDisplaySize(data: number): number{
         return data > 100 ? 100 : data
     }
+    const allCards: FlashCard[] = characterSRSstate.cards.sort(function sortByCardNumbers(a: FlashCard, b: FlashCard){if (a.cardNumber < b.cardNumber) {return -1; }if (a.cardNumber > b.cardNumber) {return 1;}return 0;})
 
     const [displayChars, setDisplayChars] = useState<FlashCard[]>([])
     const [numberToDisplay, setNumberToDisplay] = useState<number>(prepareNumberToDisplaySize(characterSRSstate.cards.length))
 
+    function sortbyIndexNumberAscendingInclUnknown() {
+        const sortedByNumber: FlashCard[] = allCards.sort(function sortSmallToLarge(a: FlashCard, b: FlashCard){if (a.cardNumber < b.cardNumber) {return -1; }if (a.cardNumber > b.cardNumber) {return 1;}return 0;})
+        setDisplayChars(sortedByNumber.slice(0,numberToDisplay))
+    }
+    function removeUnknown(input: FlashCard[]): FlashCard[] {
+        const onlyKnown: FlashCard[] = input.filter(x=>x.repetitionValue>0)
+        return onlyKnown
+    }
+
+
     function sortbyIndexNumberAscending() {
-        const sortedByNumber: FlashCard[] = characterSRSstate.cards.sort(function sortSmallToLarge(a: FlashCard, b: FlashCard){if (a.cardNumber < b.cardNumber) {return -1; }if (a.cardNumber > b.cardNumber) {return 1;}return 0;})
+        const sortedByNumber: FlashCard[] = removeUnknown(allCards).sort(function sortSmallToLarge(a: FlashCard, b: FlashCard){if (a.cardNumber < b.cardNumber) {return -1; }if (a.cardNumber > b.cardNumber) {return 1;}return 0;})
         setDisplayChars(sortedByNumber.slice(0,numberToDisplay))
     }
     function sortbyIndexNumberDescending() {
-        const sortedByNumber: FlashCard[] = characterSRSstate.cards.sort(function sortToLargeToSmall(a: FlashCard, b: FlashCard){if (a.cardNumber < b.cardNumber) {return 1; }if (a.cardNumber > b.cardNumber) {return -1;}return 0;})
+        const sortedByNumber: FlashCard[] = removeUnknown(allCards).sort(function sortToLargeToSmall(a: FlashCard, b: FlashCard){if (a.cardNumber < b.cardNumber) {return 1; }if (a.cardNumber > b.cardNumber) {return -1;}return 0;})
         setDisplayChars(sortedByNumber.slice(0,numberToDisplay))
     }
 
     function sortByReviewNumberAscending() {
-        const sortedByReviewValue: FlashCard[] = characterSRSstate.cards.sort(function sortSmallToLarge(a: FlashCard, b: FlashCard){if (a.repetitionValue < b.repetitionValue) {return -1; }if (a.repetitionValue > b.repetitionValue) {return 1;}return 0;})
+        const sortedByReviewValue: FlashCard[] = removeUnknown(allCards).sort(function sortSmallToLarge(a: FlashCard, b: FlashCard){if (a.repetitionValue < b.repetitionValue) {return -1; }if (a.repetitionValue > b.repetitionValue) {return 1;}return 0;})
         setDisplayChars(sortedByReviewValue.slice(0,numberToDisplay))
     }
     function sortByReviewNumberDescending() {
-        const sortedByReviewValue: FlashCard[] = characterSRSstate.cards.sort(function sortLargeToSmall(a: FlashCard, b: FlashCard){if (a.repetitionValue < b.repetitionValue) {return 1; }if (a.repetitionValue > b.repetitionValue) {return -1;}return 0;})
+        const sortedByReviewValue: FlashCard[] = removeUnknown(allCards).sort(function sortLargeToSmall(a: FlashCard, b: FlashCard){if (a.repetitionValue < b.repetitionValue) {return 1; }if (a.repetitionValue > b.repetitionValue) {return -1;}return 0;})
         setDisplayChars(sortedByReviewValue.slice(0,numberToDisplay))
     }
 
     function sortByLastReviewDateAscending() {
-        const sortedByLastReviewDate: FlashCard[] = characterSRSstate.cards.sort(function sortSmallToLarge(a: FlashCard, b: FlashCard){if (a.dateOfLastReview < b.dateOfLastReview) {return -1; }if (a.dateOfLastReview > b.dateOfLastReview) {return 1;}return 0;})
+        const sortedByLastReviewDate: FlashCard[] = removeUnknown(allCards).sort(function sortSmallToLarge(a: FlashCard, b: FlashCard){if (a.dateOfLastReview < b.dateOfLastReview) {return -1; }if (a.dateOfLastReview > b.dateOfLastReview) {return 1;}return 0;})
         setDisplayChars(sortedByLastReviewDate.slice(0,numberToDisplay))
     }
     function sortByLastReviewDateDescending() {
-        const sortedByLastReviewDate: FlashCard[] = characterSRSstate.cards.sort(function sortLargeToSmall(a: FlashCard, b: FlashCard){if (a.dateOfLastReview < b.dateOfLastReview) {return 1; }if (a.dateOfLastReview > b.dateOfLastReview) {return -1;}return 0;})
+        const sortedByLastReviewDate: FlashCard[] = removeUnknown(allCards).sort(function sortLargeToSmall(a: FlashCard, b: FlashCard){if (a.dateOfLastReview < b.dateOfLastReview) {return 1; }if (a.dateOfLastReview > b.dateOfLastReview) {return -1;}return 0;})
         setDisplayChars(sortedByLastReviewDate.slice(0,numberToDisplay))
     }
 
@@ -65,14 +74,15 @@ const DisplayAllHeisig: React.FunctionComponent<IPage> = props => {
         <p>number of heisig items: {characterSRSstate.cards.length}</p>
         <button type="button" onClick={toggleSize}>toggleSize{numberToDisplay}</button>
         <button type="button" onClick={clearData}>clearData</button>
+        <button type="button" onClick={sortbyIndexNumberAscendingInclUnknown}>sortAllCardsByCharNumberAscending</button>
         <p></p>
-        <button type="button" onClick={sortbyIndexNumberAscending}>sortByCharNumberAscending</button>
-        <button type="button" onClick={sortByReviewNumberAscending}>sortByReviewValueAscending</button>
-        <button type="button" onClick={sortByLastReviewDateAscending}>sortByLastReviewDateAscending</button>
+        <button type="button" onClick={sortbyIndexNumberAscending}>sortKnownCardsByCharNumberAscending</button>
+        <button type="button" onClick={sortByReviewNumberAscending}>sortKnownCardsByReviewValueAscending</button>
+        <button type="button" onClick={sortByLastReviewDateAscending}>sortKnownCardsByLastReviewDateAscending</button>
         <p></p>
-        <button type="button" onClick={sortbyIndexNumberDescending}>sortByCharNumberDescending</button>
-        <button type="button" onClick={sortByReviewNumberDescending}>sortByReviewValueDescending</button>
-        <button type="button" onClick={sortByLastReviewDateDescending}>sortByLastReviewDateDescending</button>
+        <button type="button" onClick={sortbyIndexNumberDescending}>sortKnownCardsByCharNumberDescending</button>
+        <button type="button" onClick={sortByReviewNumberDescending}>sortKnownCardsByReviewValueDescending</button>
+        <button type="button" onClick={sortByLastReviewDateDescending}>sortKnownCardsByLastReviewDateDescending</button>
         <Todos data={displayChars}/>
     </section>
     //return <h1> The display all heisig page </h1>
