9ca3c0ddb86d73dbbb0f28065b3913479c6d4999
Weiqifan1 <cmlykke@hotmail.com>
2021-11-07 20:02:06 +0100

    20211107_20.01: added tags to all cards and each card

--- src/state/state-types/charactersrstypes.ts
+++ src/state/state-types/charactersrstypes.ts
@@ -10,11 +10,15 @@ export interface FlashCard {
     notableCards: number[];
     dateOfLastReview: string;
     repetitionValue: number;
+    repetitionHistory: number[];
+    tags: string[];
 }
 
+//case class HeisigCollection(deckName: String, tags:Map[String, String], cards: Array[HeisigObj])
 export interface FlashCardDeck {
     deckName: string;
     deckInfo: string;
     settings: Map<string, string>;
+    tags:  Map<string, string>;
     cards: FlashCard[];
 }
